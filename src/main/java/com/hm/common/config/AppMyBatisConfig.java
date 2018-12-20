06b854c41595541a8d36295a7de1865c0cf32721
liahua <85178516@qq.com>
2018-12-20 13:23:46 +0800

    更新分页查询插件

--- src/main/java/com/hm/common/config/AppMyBatisConfig.java
+++ src/main/java/com/hm/common/config/AppMyBatisConfig.java
@@ -1,6 +1,7 @@
 package com.hm.common.config;
 
 import java.io.IOException;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
@@ -11,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import com.github.pagehelper.PageHelper;
 /**
  * mybatis整合配置
  * @author lord
@@ -23,13 +26,27 @@ public class AppMyBatisConfig {
 	 public SqlSessionFactoryBean newSqlSessionFactoryBean(
 			 @Autowired DataSource dataSource) throws IOException{
 		 SqlSessionFactoryBean fBean=new SqlSessionFactoryBean();
+		 
+		 //configuration配置驼峰规则
 		 org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
 		 configuration.setMapUnderscoreToCamelCase(true);
+		
+		 //configuration配置分页查询插件
+		 PageHelper pageHelper = new PageHelper();
+		 Properties prop = new Properties();
+		 prop.setProperty("dialect", "mysql");
+		 pageHelper.setProperties(prop);
+		 configuration.addInterceptor(pageHelper);
+		 
+		 //导入configuration
 		 fBean.setConfiguration(configuration);
+		 //导入数据源
 		 fBean.setDataSource(dataSource);
+		 //关联映射文件
 		 Resource[] mapperLocations=
 new PathMatchingResourcePatternResolver()
 		 .getResources("classpath:mapper/sys/*.xml");
+		 //导入映射文件
 		 fBean.setMapperLocations(mapperLocations);
 		 return fBean;
 	 }

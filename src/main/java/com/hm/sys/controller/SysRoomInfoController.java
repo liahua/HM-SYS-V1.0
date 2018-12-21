ad3705b941862d947a8081c84caa2b05cc7e3d27
Genesislines <sccode@outlook.com>
2018-12-21 00:10:03 -0500

    删除了人RoomView控制类

--- src/main/java/com/hm/sys/controller/SysRoomInfoController.java
+++ src/main/java/com/hm/sys/controller/SysRoomInfoController.java
@@ -17,55 +17,59 @@ import com.hm.sys.service.SysRoomInfoService;
 @Controller
 @RequestMapping("/room/")
 public class SysRoomInfoController {
-	
+
 	@Autowired
-	  private SysRoomInfoService sysRoomInfoService;
-	
+	private SysRoomInfoService sysRoomInfoService;
+
 	@RequestMapping("doRoomInfoUI")
 	public String doRoomInfoUI(){
-	return "room/room_info";
+		return "room/room_info";
 	}
 
-	  /**
-	   * 执行日志删除操作
-	   * @param ids
-	   * @return
-	   * log/doDeleteObjects.do?ids=70,71
-	   */
-	  @RequestMapping("doDeleteRoomInfo")
-	  @ResponseBody
-	  public JsonResult doDeleteRoomInfo(Integer... ids) {
-		  sysRoomInfoService.deleteRoomInfo(ids);
+	/**
+	 * 执行日志删除操作
+	 * @param ids
+	 * @return
+	 * log/doDeleteObjects.do?ids=70,71
+	 */
+	@RequestMapping("doDeleteRoomInfo")
+	@ResponseBody
+	public JsonResult doDeleteRoomInfo(Integer... ids) {
+		sysRoomInfoService.deleteRoomInfo(ids);
 		return new JsonResult("delete ok");
 	}
-	  //../log/doFindPageObjects.do?pageCurrent=1
-	  @RequestMapping("doFindPageObjectsInfo")
-	  @ResponseBody
-	  public JsonResult doFindPageObjectsInfo(   
-			  String roomName,
-			  Integer pageCurrent){
-		  PageObject<RoomInfo> pageObject=
-				  sysRoomInfoService.findPageObjects(roomName,
-							 pageCurrent);
-		  return new JsonResult(pageObject);		  
-}
-	  
-	  //将方法返回值转换为json格式的字符串返回
-	  //在将对象转换为json格式字符串时会调用对象的get方法获取值
-	  
-	  @ExceptionHandler(IllegalArgumentException.class)
-	  @ResponseBody
-	  public JsonResult doHandleException(
-			  IllegalArgumentException e){
-		  System.out.println("SysRoomInfoController.doHandleException");
-		  e.printStackTrace();
-		  return new JsonResult(e);
-	  }
-	  
-	  @RequestMapping("doFindObjectsInfo")
-		@ResponseBody
-		public JsonResult doFindObjectsInfo(RoomInfoExample example){
-			 return new JsonResult(sysRoomInfoService.findObjectsInfo(example));
-}
-	  
+	//../log/doFindPageObjects.do?pageCurrent=1
+	@RequestMapping("doFindPageObjectsInfo")
+	@ResponseBody
+	public JsonResult doFindPageObjectsInfo(   
+			String roomName,
+			Integer pageCurrent){
+		PageObject<RoomInfo> pageObject=
+				sysRoomInfoService.findPageObjects(roomName,
+						pageCurrent);
+		return new JsonResult(pageObject);		  
+	}
+
+	//将方法返回值转换为json格式的字符串返回
+	//在将对象转换为json格式字符串时会调用对象的get方法获取值
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	@ResponseBody
+	public JsonResult doHandleException(
+			IllegalArgumentException e){
+		System.out.println("SysRoomInfoController.doHandleException");
+		e.printStackTrace();
+		return new JsonResult(e);
+	}
+
+	@RequestMapping("doFindObjectsInfo")
+	@ResponseBody
+	public JsonResult doFindObjectsInfo(RoomInfoExample example){
+		return new JsonResult(sysRoomInfoService.findObjectsInfo(example));
+	}
+	@RequestMapping("doRoomViewUI")
+	public String doRoomViewUI(){
+		return "room/room_view";
+	}
+
 }

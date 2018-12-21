7ad5352e0c9c553fc3e465b0f1e0d6f86d03f1da
liahua <85178516@qq.com>
2018-12-21 21:05:49 +0800

    增加customerService roomService方法

--- src/main/java/com/hm/sys/service/SysCustomerService.java
+++ src/main/java/com/hm/sys/service/SysCustomerService.java
@@ -6,22 +6,57 @@ import com.hm.common.vo.CustomerAllInfo;
 import com.hm.common.vo.PageObject;
 import com.hm.sys.entity.CustomerInfo;
 import com.hm.sys.entity.CustomerTypeExample;
+
 /**
  * 
  * @author 叶云彤
  *
+ *
  */
 public interface SysCustomerService {
-	  List<CustomerInfo> selectByExample(CustomerTypeExample example);
-	  
-	  /**
-	      * 本方法中要分页查询角色信息,并查询角色总记录数据
-	      * @param pageCurrent 当表要查询的当前页的页码值
-	      * @return 封装当前实体数据以及分页信息
-	      */
-		 PageObject<CustomerAllInfo> findPageObjects(
-				 String name,Integer pageCurrent);
-		 
+	List<CustomerInfo> selectByExample(CustomerTypeExample example);
 
-	  
+	/**
+	 * 本方法中要分页查询角色信息,并查询角色总记录数据
+	 * 
+	 * @param pageCurrent 当表要查询的当前页的页码值
+	 * @return 封装当前实体数据以及分页信息
+	 */
+	PageObject<CustomerAllInfo> findPageObjects(String name, Integer pageCurrent);
+	
+	
+	/**
+	 * 查找通过id查找CustomerInfo
+	 * @author 李志学
+	 * @param id
+	 * @return
+	 */
+	public CustomerInfo findCustomerInfo(Integer id) ;
+	
+	/**
+	 * @author 李志学
+	 * @param customerInfo 至少包含id|name&&telephone|cardtypeId&&cardNum
+	 * @return
+	 */
+	public CustomerInfo findCustomerInfo(CustomerInfo customerInfo);
+	
+	/**
+	 * 通过name,telephone查找customerInfo
+	 * @author 李志学
+	 * @param customerName
+	 * @param telephone
+	 * @return
+	 */
+	public CustomerInfo findCustomerInfo(String customerName, String telephone);
+	
+	/**
+	 * 通过cardTypeId,cardNum查找customerInfo
+	 * @author 李志学
+	 * @param cardTypeId
+	 * @param cardNum
+	 * @return
+	 */
+	public CustomerInfo findCustomerInfo(int cardTypeId, String cardNum) ;
+	
+
 }
--- src/main/java/com/hm/sys/service/impl/SysCheckOutServiceImpl.java
+++ src/main/java/com/hm/sys/service/impl/SysCheckOutServiceImpl.java
@@ -4,33 +4,34 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+
 
 import com.github.pagehelper.StringUtil;
 import com.hm.common.exception.ServiceException;
-import com.hm.common.utils.IntegerUtil;
+
 import com.hm.common.utils.ListUtil;
-import com.hm.common.utils.ObjectUtil;
+
 import com.hm.common.vo.CheckOutVoDetails;
 import com.hm.sys.dao.CustomerInfoMapper;
 import com.hm.sys.dao.RoomInfoMapper;
 import com.hm.sys.dao.StayInfoMapper;
 import com.hm.sys.entity.CustomerInfo;
-import com.hm.sys.entity.CustomerInfoExample;
+
 import com.hm.sys.entity.RoomInfo;
 import com.hm.sys.entity.RoomInfoExample;
-import com.hm.sys.entity.RoomInfoExample.Criteria;
+import com.hm.sys.entity.StayInfo;
 import com.hm.sys.service.SysCheckOutService;
+import com.hm.sys.service.SysCustomerService;
+import com.hm.sys.service.SysRoomInfoService;
 
 @Service
 public class SysCheckOutServiceImpl implements SysCheckOutService {
 
+
 	@Autowired
-	private RoomInfoMapper roomInfoMapper;
+	private SysCustomerService sysCustomerService;
 	@Autowired
-	private StayInfoMapper stayInfoMapper;
-	@Autowired
-	private CustomerInfoMapper customerInfoMapper;
+	private SysRoomInfoService sysRoomInfoService;
 
 	@Override
 	public CheckOutVoDetails checkOutDepencyRoomNameIdCustomerInfo(CheckOutVoDetails checkOutVoDetail) {
@@ -47,121 +48,33 @@ public class SysCheckOutServiceImpl implements SysCheckOutService {
 			throw new ServiceException("房间号不正确");
 		}
 		// 查找符合roomNameId的房间的信息
-		RoomInfo roomInfo = findRoomFromRoomInfo(roomNameId);
+		RoomInfo roomInfo = sysRoomInfoService.findRoomFromRoomInfo(roomNameId);
 
 //		通过CardTypeIdCardNum查找CustomerInfo
 //		通过CustomerNameTelephone查找CustomerInfo
 //		通过id查找CustomerInfo
-		CustomerInfo customerInfo = findCustomerInfo(checkOutVoDetail.getCustomerInfo());
-		// 通过customerInfo,roomInfo查找入住且未支付信息
-
+		CustomerInfo customerInfo = sysCustomerService.findCustomerInfo(checkOutVoDetail.getCustomerInfo());
+		// 通过customerInfo查找入住信息
+		List<StayInfo> stayInfo=findStayInfo(customerInfo);
+		
 		checkOutVoDetail.setRoomInfo(roomInfo);
 		checkOutVoDetail.setCustomerInfo(customerInfo);
 		return null;
 	}
 
-	private CustomerInfo findCustomerInfo(CustomerInfo customerInfo) {
-		CustomerInfo customerInfoReal = new CustomerInfo();
-		Integer id = customerInfo.getId();
-		/**
-		 * id合法
-		 */
-		if (!IntegerUtil.isIllegality(id)) {
-			customerInfoReal = findCustomerInfo(id);
-			if (!ObjectUtil.isEmpty(customerInfoReal)) {
-				return customerInfoReal;
-			}
-		}
-		int cardType = customerInfo.getCardTypeId();
-		String cardNum = customerInfo.getCardNum();
-		/**
-		 * cardType||cardNum为空
-		 */
-		if ((!IntegerUtil.isIllegality(cardType)) && (!StringUtils.isEmpty(cardNum))) {
-			customerInfoReal = findCustomerInfo(cardType, cardNum);
-			if (!ObjectUtil.isEmpty(customerInfoReal)) {
-				return customerInfoReal;
-			}
-		}
-
-		String customerName = customerInfo.getCustomerName();
-		String telephone = customerInfo.getTelephone();
-		if ((!StringUtils.isEmpty(customerName)) && (!StringUtils.isEmpty(telephone))) {
-			customerInfoReal = findCustomerInfo(customerName, telephone);
-			if (!ObjectUtil.isEmpty(customerInfoReal)) {
-				return customerInfoReal;
-			}
-
-		}
-		// TODO
-		throw new ServiceException("无法找到客户信息,请重新输入");
-	}
-/**
- * 查找CustomerInfo
- * @param customerName
- * @param telephone
- * @return
- */
-	public CustomerInfo findCustomerInfo(String customerName, String telephone) {
-		CustomerInfoExample customerInfoExample = new CustomerInfoExample();
-		customerInfoExample.or().andCustomerNameEqualTo(customerName).andTelephoneEqualTo(telephone);
-		List<CustomerInfo> customerInfo = customerInfoMapper.selectByExample(customerInfoExample);
-		if (ListUtil.isEmpty(customerInfo)) {
-			throw new ServiceException("无此客户信息");
-		} else if (ListUtil.count(customerInfo) > 1) {
-			throw new ServiceException("存在两个或两个以上客户姓名与电话号码相同状况");
-		}
-		return customerInfo.get(1);
-	}
-/**
- * 查找CustomerInfo
- * @param cardTypeId
- * @param cardNum
- * @return
- */
-	public CustomerInfo findCustomerInfo(int cardTypeId, String cardNum) {
-		CustomerInfoExample customerInfoExample = new CustomerInfoExample();
-		customerInfoExample.or().andCardTypeIdEqualTo(cardTypeId).andCardNumEqualTo(cardNum);
-		List<CustomerInfo> customerInfo = customerInfoMapper.selectByExample(customerInfoExample);
-		if (ListUtil.isEmpty(customerInfo)) {
-			throw new ServiceException("无此客户信息");
-		} else if (ListUtil.count(customerInfo) > 1) {
-			throw new ServiceException("存在两个或两个以上客户信息证件号证件密码一致情况");
-		}
-		return customerInfo.get(1);
-	}
-/**
- * 查找CustomerInfo
- * @param id
- * @return
- */
-	private CustomerInfo findCustomerInfo(Integer id) {
-		CustomerInfo CustomerInfo = customerInfoMapper.selectByPrimaryKey(id);
-		if (ObjectUtil.isEmpty(CustomerInfo)) {
-			throw new ServiceException("无此客户id");
-		}
-		return CustomerInfo;
+	private List<StayInfo> findStayInfo(CustomerInfo customerInfo) {
+		
+		return null;
 	}
 
-	/**
-	 * 
-	 * @param roomNameId
-	 * @return 返回所有拥有roomNameId的房间信息
-	 */
-	public RoomInfo findRoomFromRoomInfo(String roomNameId) {
-		RoomInfoExample roomInfoExample = new RoomInfoExample();
-		roomInfoExample.or().andRoomNameEqualTo(roomNameId);
-		List<RoomInfo> roomsInfo = roomInfoMapper.selectByExample(roomInfoExample);
+	
 
-		if (ListUtil.isEmpty(roomsInfo)) {
-			throw new ServiceException("无此房间");
-		} else if (ListUtil.count(roomsInfo) > 1) {
-			throw new ServiceException("房间名重复,联系管理员查改");
-		}
-		RoomInfo roomInfo = roomsInfo.get(1);
+	
 
-		return roomInfo;
-	}
+
+
+
+
 
 	@Override
 	public CheckOutVoDetails checkOutDepencyOrderInfo(CheckOutVoDetails CheckOutVoDetails) {

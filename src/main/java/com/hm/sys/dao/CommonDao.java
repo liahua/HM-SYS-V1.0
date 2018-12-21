21840ab91ac5a220a4f8da92bcbc1251ffef76a7
hanxixideningshi <1254193445@qq.com>
2018-12-21 21:30:58 +0800

    叶云彤

---
+++ src/main/java/com/hm/sys/dao/CommonDao.java
@@ -0,0 +1,36 @@
+package com.hm.sys.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.hm.common.vo.CustomerAllInfo;
+import com.hm.sys.entity.CustomerInfo;
+import com.hm.sys.entity.CustomerInfoExample;
+
+
+public interface CommonDao {
+	/**
+	 * list集合查询封装
+	 * @param 
+	 * @return
+	 */
+	    List<CustomerInfo> selectByExample(CustomerInfoExample example);
+	    /**
+	     * 分页查询角色信息
+	     * @param startIndex 上一页的结束位置
+	     * @param pageSize 每页要查询的记录数
+	     * @return
+	     */
+		
+		List<CustomerAllInfo> findPageObjects(
+	             @Param("name")String name,
+				@Param("startIndex")Integer startIndex,
+				@Param("pageSize")Integer pageSize);
+		/**
+		 * 查询记录总数
+		 * @return
+		 */
+		int getRowCount(@Param("name")String name);
+
+}

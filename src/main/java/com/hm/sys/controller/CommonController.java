9f75d85e237a0e7f84b9e0b83dedf9ae91e06610
hanxixideningshi <1254193445@qq.com>
2018-12-21 14:04:41 +0800

    叶云彤 加注释

--- src/main/java/com/hm/sys/controller/CommonController.java
+++ src/main/java/com/hm/sys/controller/CommonController.java
@@ -8,18 +8,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.hm.common.vo.JsonResult;
 import com.hm.sys.entity.CustomerTypeExample;
 import com.hm.sys.service.SysCustomerService;
-
+/**
+ * 
+ * 客户管理  
+ * @author 叶云彤
+ *
+ */
 @Controller
 @RequestMapping("/common/")
 public class CommonController {
-	
+
 	@Autowired
 	private SysCustomerService sysCustomerService;
-	
+	/**
+	 * 普通客户
+	 * 
+	 * @return
+	 */
 	@RequestMapping("doCommonUI")
 	public String doCommonUI() {
 		return "customer/customer";
 	}
+	/**
+	 * 
+	 * 显示页面数据
+	 * @param example
+	 * @return
+	 */
 	@RequestMapping("selectByExample")
 	@ResponseBody
 		public JsonResult selectByExample(CustomerTypeExample example) {
--- src/main/java/com/hm/sys/service/SysCustomerService.java
+++ src/main/java/com/hm/sys/service/SysCustomerService.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import com.hm.sys.entity.CustomerInfo;
 import com.hm.sys.entity.CustomerTypeExample;
-
+/**
+ * 
+ * @author 叶云彤
+ *
+ */
 public interface SysCustomerService {
 	  List<CustomerInfo> selectByExample(CustomerTypeExample example);
 }

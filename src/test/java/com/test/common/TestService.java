23aca06752532ee2fb6d6178c0a487ef171d7acc
tarena <tarena@tarena-pc>
2018-12-23 16:28:14 +0800

    优化报表小数显示

--- src/test/java/com/test/common/TestService.java
+++ src/test/java/com/test/common/TestService.java
@@ -1,7 +1,18 @@
 package com.test.common;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
 import org.junit.Test;
 
+import com.hm.sys.dao.AccountInfoMapper;
+import com.hm.sys.entity.AccountInfo;
+import com.hm.sys.entity.AccountInfoExample;
+import com.hm.sys.entity.AccountInfoExample.Criteria;
 import com.hm.sys.service.impl.SysAccountServiceImpl;
 
 public class TestService extends TestBase{
@@ -12,5 +23,53 @@ public class TestService extends TestBase{
 		for(int i =1 ;i<=31;i++) {
 			ac.doDayCheck("2018-12-"+i);
 		}
+	}
+	
+	@Test
+	public void doTestTable() {
+		AccountInfoMapper bean = ctx.getBean(AccountInfoMapper.class);
+		
+		AccountInfoExample aie = new AccountInfoExample();
+		Criteria cc = aie.createCriteria();
+		cc.andDayBetween(start(), end());
+		
+		List<AccountInfo> list = bean.selectByExample(aie);
+		
+		System.out.println(list.size());
+		for(AccountInfo a : list) {
+			AccountInfoExample aie1 = new AccountInfoExample();
+			Criteria cc1 = aie1.createCriteria();
+			cc1.andIdEqualTo(a.getId());
+			
+			AccountInfo ai = new AccountInfo();
+			ai.setId(a.getId());
+			ai.setDay(a.getDay());
+			ai.setManCount(a.getManCount());
+			ai.setOrderCount(new Random().nextInt(100));
+			ai.setOrderMoney(BigDecimal.valueOf(1000d+new Random().nextDouble()).setScale(2,BigDecimal.ROUND_HALF_DOWN).doubleValue());
+			ai.setCashCount(BigDecimal.valueOf(1000d+new Random().nextDouble()).setScale(2,BigDecimal.ROUND_HALF_DOWN).doubleValue());
+			ai.setCheckinCount(BigDecimal.valueOf(1000d+new Random().nextDouble()).setScale(2,BigDecimal.ROUND_HALF_DOWN).doubleValue());
+			
+			bean.updateByExample(ai, aie1);
+		}
+		
+	}
+
+	private Date end() {
+		Calendar c = Calendar.getInstance(Locale.CHINA);
+		c.set(Calendar.YEAR,2018);
+		c.set(Calendar.MONTH,11);
+		c.set(Calendar.DAY_OF_MONTH,30);
+		System.out.println(c.getTime());
+		return c.getTime();
+	}
+
+	private Date start() {
+		Calendar c = Calendar.getInstance(Locale.CHINA);
+		c.set(Calendar.YEAR,2018);
+		c.set(Calendar.MONTH,11);
+		c.set(Calendar.DAY_OF_MONTH,20);
+		System.out.println(c.getTime());
+		return c.getTime();
 	}
 }
